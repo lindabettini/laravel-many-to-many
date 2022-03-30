43430533f6091354c0d6643a8036edfc8c57f2cf
lindabettini <linda.bettini@hotmail.it>
2022-03-31 00:24:50 +0200

    add table

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\Tag;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $tags_names = ['FrontEnd', 'Backend', 'FullStack', 'UI/UX', 'Design', 'CMS'];
+
+        foreach ($tags_names as $tag) {
+            $tag = new Tag();
+            $tag->label = $tag;
+            $tag->color = $faker->hexColor();
+            $tag->save();
+        }
+    }
+}
