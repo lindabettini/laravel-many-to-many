9d0f3983603119c41e0ea1a960189f8da455993e
lindabettini <linda.bettini@hotmail.it>
2022-04-05 17:18:34 +0200

    add preview image

--- public/js/image-preview.js
+++ public/js/image-preview.js
@@ -2,7 +2,14 @@ const placeholder = "https://socialistmodernism.com/wp-content/uploads/2017/07/p
 const imageInput = document.getElementById('image');
 const imagePreview = document.getElementById('preview');
 
-imageInput.addEventListener('change', e => {
-  const preview = imageInput.value ?? placeholder;
-  imagePreview.setAttribute('src', preview);
+imageInput.addEventListener('change', () => {
+  //<Controllo se un file è stato effettivamente caricato
+  if(imageInput.files && imageInput.files[0]){
+    let reader = new FileReader();
+    reader.readAsDataURL(imageInput.files[0]);
+
+    reader.onload = (e) => {
+      imagePreview.setAttribute('src', e.target.result);
+    }
+  }
 })
